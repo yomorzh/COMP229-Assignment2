cdf55803e67a610b388ac12ef5ee04499a016fa1
Aleksandr Shelukheev <shelukheev@gmail.com>
2021-02-28 23:19:27 -0500

    added not working mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/contacts_list"
+    //"URI": "mongodb://localhost/contacts_list"
+    "URI": "mongodb+srv://aleksandr:<Basurm@n12345>@mongodbserver.uwkk5.mongodb.net/contacts_list?retryWrites=true&w=majority"
 };
